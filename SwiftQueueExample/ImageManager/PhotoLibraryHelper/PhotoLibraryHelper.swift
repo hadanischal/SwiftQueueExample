bd1a7e56c1a6d1f77ffa367eeede63c3954f85b0
Nischal Hada <nischal@moroku.com>
2019-07-30 10:09:31 +1000

    Implemented PhotoLibraryHelper for PHPhotoLibrary authorizationStatus

---
+++ SwiftQueueExample/ImageManager/PhotoLibraryHelper/PhotoLibraryHelper.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoLibraryHelper.swift
+//  SwiftQueueExample
+//
+//  Created by Nischal Hada on 7/29/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import Foundation
+import Photos
+import RxSwift
+
+class PhotoLibraryHelper: PhotoLibraryHelperProtocol {
+    func authorizationStatus() -> Single<PhotoAuthorizationStatus> {
+        return Single<PhotoAuthorizationStatus>.create { single in
+           let status =  PHPhotoLibrary.authorizationStatus()
+            switch status {
+            case .notDetermined:
+                single(.success(.notDetermined))
+            case .restricted:
+                single(.success(.restricted))
+            case .denied:
+                single(.success(.denied))
+            case .authorized:
+                single(.success(.authorized))
+            @unknown default:
+                single(.error(RxError.unknown))
+                fatalError("PHPhotoLibrary is not available on this version of OS.")
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func requestAuthorization() -> Single<Bool> {
+        return Single<Bool>.create { single in
+            PHPhotoLibrary.requestAuthorization { status in
+                switch status {
+                    case .authorized:
+                    single(.success(true))
+                 default:
+                    single(.success(false))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
