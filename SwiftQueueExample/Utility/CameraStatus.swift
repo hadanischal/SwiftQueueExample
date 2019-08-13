8ad2448f9956f438508b4d33faeadfde2e75f067
Nischal Hada <nischal.hada@voltbank.com.au>
2019-08-14 08:53:53 +1000

    Implemented AVFoundationHelper class

---
+++ SwiftQueueExample/Utility/CameraStatus.swift
@@ -0,0 +1,16 @@
+//
+//  CameraStatus.swift
+//  SwiftQueueExample
+//
+//  Created by Nischal Hada on 13/8/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+public enum CameraStatus {
+    case notDetermined
+    case restricted
+    case denied
+    case authorized
+}
